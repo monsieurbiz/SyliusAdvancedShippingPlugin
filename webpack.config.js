9868a80915f23dc217a5b57cac62eb1609be630e
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2024-09-27 15:26:53 +0200

    fix(webpack): update config and packages to build assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
 const Encore = require('@symfony/webpack-encore');
 const path = require('path');
 
-({
-  from: './assets/images',
-  to: 'images/[path][name].[ext]',
-} |> Encore
+const libAssets = path.resolve(__dirname, 'assets/js/lib');
+
+Encore
+  // directory where compiled assets will be stored
   .setOutputPath('src/Resources/public')
+  // public path used by the web server to access the output path
   .setPublicPath('/public')
 
+  // entries
   .addEntry('advanced-shipping', './assets/js/app.js')
 
+  // copy images
+  .copyFiles({
+    from: './assets/images',
+    to: 'images/[path][name].[ext]',
+    includeSubdirectories: true,
+    pattern: /\.(png|jpg|jpeg|svg)$/
+  })
+
+  // configuration
   .disableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
   .enableVersioning(Encore.isProduction())
-  .addAliases({
-    '@lib': path.resolve(__dirname, 'assets/js/lib/'),
-  })
 
-  .copyFiles)
-
+  // organise files
   .configureFilenames({
     js: 'js/[name].js',
     css: 'css/[name].css',
-  });
-module.exports = Encore.getWebpackConfig();
+  })
+  .configureImageRule({
+    type: 'asset',
+    filename: `images/[name].[ext]`,
+  })
+;
+
+const config = Encore.getWebpackConfig();
+config.resolve.alias = {
+  '@lib': libAssets,
+};
+
+module.exports = config;
